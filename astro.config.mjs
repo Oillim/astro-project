f07a79e0976b62fb3770c40260f72246722e3254
Oillim <111446926+Oillim@users.noreply.github.com>
2024-08-25 14:03:53 +0000

    template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-
-import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [tailwind(), alpinejs()],
+  output: 'static'
 });
